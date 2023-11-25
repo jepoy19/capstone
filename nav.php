a202b1d64f4fa40b091431bb565284587037beee
jepoy19 <depyok609@gmail.com>
2023-11-25 23:01:13 +0800

    first commit

---
+++ nav.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/nav.css">
+    <title>Nav</title>
+</head>
+<body>
+    <?php
+    include_once("index.php");
+    ?>
+<div id="nav">   
+    <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid" >
+        <a class="navbar-brand" href="#">
+            <img src="assets/CPSU-LOGO.png" alt="Bootstrap" width="30" height="24">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+
+        </ul>
+        </div>
+    </div>
+    </nav>
+</div> 
+</body>
+</html>
